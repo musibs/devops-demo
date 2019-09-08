48e9182d5ca7a7a8a1cb7f66d83fc3fe093c9970
musibs <musib.somnath@gmail.com>
2019-09-08 11:50:04 +1000

    Initial Set up along with SCM and distribution management configurations

---
+++ src/main/java/com/codefountain/spring/web/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.codefountain.spring.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+	@GetMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("Course", "DevOps Demo");
+		return "index";
+	}
+	
+}
---
+++ src/test/java/com/codefountain/spring/web/TestWelcomeController.java
@@ -0,0 +1,39 @@
+package com.codefountain.spring.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class TestWelcomeController {
+
+	private MockMvc mockMvc;
+	
+	@Autowired
+	WebApplicationContext webApplicationContext;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Test
+	public void testWelcome() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk())
+		.andExpect(model().attribute("Course", "DevOps Demo"));
+	}
+
+}
